1a28b97376fd38124092d622594126ab888fae76
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2024-05-17 18:00:31 +0300

    feat: :lipstick: Add hover and transition effects to ProductCard component

--- app/_components/ProductCard.jsx
+++ app/_components/ProductCard.jsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
+
 export default function ProductCard({ onAddToCart, ...product }) {
   return (
-    <div className='card bg-base-100 shadow-xl'>
-      <figure>
-        <img
+    <div className='card max-w-[500px] bg-base-100 shadow-xl transform transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105'>
+      <figure className='overflow-hidden'>
+        <Image
           src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
           alt='Shoes'
+          className='transition duration-500 ease-in-out transform hover:scale-100'
+          width={500}
+          height={500}
         />
       </figure>
       <div className='card-body'>
