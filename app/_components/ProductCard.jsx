b0a3a11fe02fc87bdc24198048b743056f1b03a9
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2024-05-17 20:01:12 +0300

    feat: :lipstick: Update images urls

--- app/_components/ProductCard.jsx
+++ app/_components/ProductCard.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export default function ProductCard({ onAddToCart, ...product }) {
   return (
-    <div className='card max-w-[500px] bg-base-100 shadow-xl transform transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105'>
+    <div className='card max-w-[500px] h-[500px] bg-base-100 shadow-xl transform transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105'>
       <figure className='overflow-hidden'>
         <Image
-          src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-          alt='Shoes'
-          className='transition duration-500 ease-in-out transform hover:scale-100'
+          src={product.image}
+          alt={product.name}
+          className='transition duration-500 ease-in-out transform hover:scale-100 w-auto h-auto'
           width={500}
           height={500}
         />
